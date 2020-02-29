daa398289afc97e38e7450746beda5a4588a8ffb
zhangdongaoZzz <540979352@qq.com>
2020-02-29 23:44:23 +0800

    first commit

---
+++ src/main/java/com/mxz/dsm/MxzApplication.java
@@ -0,0 +1,13 @@
+package com.mxz.dsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxz/dsm/MxzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxz.dsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
